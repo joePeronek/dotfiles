8e1d7c6ba7d649efe816a9ff3e92235f5e0f35fe
Joe Peronek <joe@peronek.com>
2025-12-02 19:16:48 -0500

    changed some binds

--- dot_config/nvim/lua/joePeronek/plugins/harpoon.lua
+++ dot_config/nvim/lua/joePeronek/plugins/harpoon.lua
@@ -9,16 +9,16 @@ return {
 		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Toggle Harpoon menu" })
 
 		-- Navigation key mappings
-		vim.keymap.set("n", "<C-n>", function()
+		vim.keymap.set("n", "<leader>z", function()
 			ui.nav_file(1)
 		end, { desc = "Navigate to file 1" })
-		vim.keymap.set("n", "<C-e>", function()
+		vim.keymap.set("n", "<leader>x", function()
 			ui.nav_file(2)
 		end, { desc = "Navigate to file 2" })
-		vim.keymap.set("n", "<C-i>", function()
+		vim.keymap.set("n", "<leader>c", function()
 			ui.nav_file(3)
 		end, { desc = "Navigate to file 3" })
-		vim.keymap.set("n", "<C-o>", function()
+		vim.keymap.set("n", "<leader>v", function()
 			ui.nav_file(4)
 		end, { desc = "Navigate to file 4" })
 	end,
